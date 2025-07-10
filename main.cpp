4490e846c0a0180e6d17c33256447eea03b39af6
KrzysTheTech <krzysztofdemirkuzniak@gmail.com>
2025-07-10 13:22:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -145,45 +145,4 @@ void handle_debian_build() {
 void handle_ubuntu_build() {
     std::map<std::string, std::string> versions = {
         {"24.04 LTS (noble)", "noble"}, {"22.04 LTS (jammy)", "jammy"}, {"20.04 LTS (focal)", "focal"},
-        {"18.04 LTS (bionic)", "bionic"}, {"25.10 (questing)", "questing"}, {"25.04 (plucky)", "plucky"},
-        {"24.10 (oracular) [EOL: July 10, 2025]", "oracular"}
-    };
-    std::vector<std::string> desktops = {"GNOME", "KDE", "XFCE", "LXQT", "No X11 (Server)"};
-
-    std::vector<std::string> version_keys;
-    for(auto const& [key, val] : versions) version_keys.push_back(key);
-
-    std::string version_key = version_keys[display_menu("Select Ubuntu Version", version_keys) - 1];
-    std::string desktop_key = desktops[display_menu("Select Desktop Environment", desktops) - 1];
-    std::string codename = versions[version_key];
-
-    std::string command = "lb config -d " + codename;
-    if (desktop_key != "No X11 (Server)") {
-        command += " --packages \"";
-        command += to_lower(desktop_key);
-        command += "-desktop\"";
-    }
-
-    std::cout << "\nTo build your Ubuntu ISO, create a directory and run the following commands inside it:" << std::endl;
-    std::cout << "-----------------------------------------------------" << std::endl;
-    std::cout << command << std::endl;
-    std::cout << "sudo lb build" << std::endl;
-    std::cout << "-----------------------------------------------------" << std::endl;
-}
-
-void handle_fedora_build() {
-    std::vector<std::string> versions = {"42", "41", "Rawhide"};
-    std::string selected_version = versions[display_menu("Select Fedora Version", versions) - 1];
-    
-    std::cout << "\nFedora " << selected_version << " selected." << std::endl;
-    std::cout << "Building a Fedora ISO requires a Kickstart (.ks) file." << std::endl;
-    std::string command = "sudo lorax --product=\"Fedora\" --version=\"" + selected_version + "\" --release=\"Fedora " + selected_version + "\" ./output fedora.ks";
-    std::cout << "Create a Kickstart file and run a command similar to this to build the ISO:\n" << command << std::endl;
-}
-
-void handle_arch_build() {
-    std::cout << "\nArch Linux is a rolling release." << std::endl;
-    std::cout << "Building an Arch Linux ISO requires a profile directory (e.g., 'releng')." << std::endl;
-    std::string command = "sudo mkarchiso -v -w /tmp/archiso-work -o . releng";
-    std::cout << "Create a profile directory and run the following command to build the ISO:\n" << command << std::endl;
-}
+        {"18.04 LTS (bionic)", "bionic"}, {"25
